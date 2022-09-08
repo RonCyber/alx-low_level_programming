65fd7d7c68c9418a9503b0246afe31ec01a85da1
RonCyber <ronnieuch@gmail.com>
2022-09-08 13:47:20 -0700

    Print text with return value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main(void)
+ *
+ *Return: Always 0 (Success)
+ */
+ int main(void)	
+{																	puts("Programming is like building a multilingual puzzle");									return(0);															}
