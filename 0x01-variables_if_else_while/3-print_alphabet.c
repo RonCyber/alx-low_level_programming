6da055172d531825a186fae34a24a4c84945645b
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-09 17:12:22 +0200

    Update 3-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -2,16 +2,15 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always returns 0 (Success)
  *
  */
 int main(void)
 {
 	char a;
 
-	for (a = 'a'; a <= 'z'; a++)
+	for (a = 'a'; a <= 'z' ; a++)
 		putchar(a);
-
 	for (a = 'A'; a <= 'Z'; a++)
 		putchar(a);
 	putchar('\n');
