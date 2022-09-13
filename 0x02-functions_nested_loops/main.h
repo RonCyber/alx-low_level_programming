5bb67f1a6e628cec284968c3bf56e29dee160122
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-13 14:21:55 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,8 @@
  * Prints -putchar followed by a new line
  * Return: Always 0 (Success)
  */
-int _putchar(int, char *);
+int _putchar(int);
+int _putchar(char *);
 /**
  * print_alphabet - function to print alphabet
  *
