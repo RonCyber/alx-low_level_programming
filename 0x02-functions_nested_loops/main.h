794e08bc48b6a74c7c9f51e145affff91d53a2dd
RonCyber <ronnieuch@gmail.com>
2022-09-13 03:14:36 -0700

    First header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef FIRSTHEADER_H
+#define FIRSTHEADER_H
+/**
+ * _putchar - writes the character c to std
+ * Prints -putchar followed by a new line
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c)
+#endif
