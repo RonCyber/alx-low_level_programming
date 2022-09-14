9ed329f769d71600bfae9d52dc5964e1488b936d
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-14 14:05:33 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 prints alphabet 10 times in lowercase folllowed by a new line
+ *
  * Return: Always 0
  */
 void print_alphabet_x10(void)
@@ -9,12 +10,12 @@ void print_alphabet_x10(void)
 char a;
 char i;
 
-for(a = 0; a <= 9; a++)
+for (a = 0; a <= 9; a++)
 {
-for(i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
-}
 _putchar('\n');
+}
 }
