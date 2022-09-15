1bf2b5bcaae5155976ba29e0b354fa6c037a20ac
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-15 23:16:07 +0200

    Create 8-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/8-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line
+ *
+ * @n: integer to print stuff?
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
