7f8441d35f19c719fae5e684de30b577d2bf7a95
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-15 21:30:27 +0200

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,32 +1,29 @@
-#include"main.h"
-
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - main function
  *
- * Description: prints the first 50 Fibonacci numbers
- *		starting with 1 and 2 folled by a new line
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: nothing
+ */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
+	int counter = 2;
+	long int a = 1;
+	long int b = a + 1;
+	long int c = a + b;
 
-	for (count = 0; count < 50; ++count)
+	printf("%ld, %ld, ", a, b);
+	while (counter < 50)
 	{
-		sum = fib1 + fib2;
-		printf("%lu", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-
-		if (count == 49)
-			printf("\n");
-		else
+		printf("%ld", c);
+		counter++;
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 50)
+		{
 			printf(", ");
+		}
 	}
-
+	printf("\n");
 	return (0);
 }
