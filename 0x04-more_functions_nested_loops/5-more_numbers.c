e38877cd65b8f7d0b89dea9d443a3be8f868625c
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-15 23:07:08 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,12 @@ void more_numbers(void)
 	for (r = 0; r <= 9; r++)
 	{
 		for (i = 0; i <= 14; i++)
-		{
-			_putchar(i);
-}
-			_putchar(r);
+		{			
+if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(r % 10 + '0');;
 	}
 	_putchar('\n');
 }
