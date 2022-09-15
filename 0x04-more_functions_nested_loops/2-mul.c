c5c754e6a1bc6cfedc5619f4ab63e838ab2ccdab
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-15 22:35:23 +0200

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies a and b
+ * @a: multiplied
+ * @b: multiplied to a
+ * Return: 0 Always
+ */
+int mul(int a, int b)
+{
+int a;
+int b;
+int c = a * b;
+_putchar(c);
+	return (0);
+}
