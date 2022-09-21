22367734f84b1135598f467fb50cd863f6d529f2
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-21 10:50:15 +0200

    Create main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,52 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _strcpy - main function
+ * @dest: destination
+ * @src: source
+ * Return: return value
+ */
+char *_strcpy(char *dest, char *src);
+/**
+ * _strncat - concat string at n times
+ * @dest: destination source
+ * @src: source of array
+ * @n: n amount of times
+ * Return: return char value
+ */
+char *_strncat(char *dest, char *src, int n);
+/**
+ * _strncpy - function to copy
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: times to copy
+ * Return: return char value
+ */
+char *_strncpy(char *dest, char *src, int n);
+/**
+ * _strcmp - compares strings
+ *
+ * @s1: first string
+ * @s2: second string to compare
+ * Return: int value
+ */
+int _strcmp(char *s1, char *s2);
+/**
+ * reverse_array - reverses an array
+ * @a: array to reverse
+ * @n: how many elements
+ */
+void reverse_array(int *a, int n);
+/**
+ * string_toupper - makes lower to upper
+ * @s: character to make upper
+ * Return: character value
+ */
+char *string_toupper(char *s);
+/**
+ * cap_string - string to capitalize;
+ * @s: string to capitalize
+ * Return: return the string
+ */
+char *cap_string(char *);
+#endif /*end of main.h*/
