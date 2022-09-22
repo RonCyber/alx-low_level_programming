b53bf7d36b39a9f717441e88c97771733af5a8dd
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-22 12:31:30 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,11 @@ int i, c;
 /*find the size of dest and src arrays and iterate them from 0 without null byte then add them*/
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-
 	for (c = 0; src[c] != '\0'; c++)
 	{
 		dest[i] = src[c];
 		i++;
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
