43dfb373b835b47da285c28cb8032f082b622c06
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-30 19:45:53 +0200

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,19 @@
-#include <unistd.h>
+#include"main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _puts - prints a string, followed by a newline to stdout
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
 {
-	return (write(1, &c, 1));
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
 }
