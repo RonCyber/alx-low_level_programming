e0c456b8fd27ef854450a7cfc6bfc0ff3e3e1d9e
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-09-30 19:41:59 +0200

    Create 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - concats two arrays
+ *
+ * @dest: destination of concat
+ * @src: concats to dest
+ * @n: amount of times to append
+ *
+ * Return: char value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+
+}
