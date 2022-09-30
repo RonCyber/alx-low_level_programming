f3c8b0d12e25bada4a3f4e0a3ae9bbef8031ef7d
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-10-01 00:21:58 +0200

    Create 1-argc.c

---
+++ 0x0A-argc_argv/1-argc.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+int args(int argc, char *argv[])
+{
+int i, count;
+
+for (i = 0, i < count, i++)
+{
+count = argc - 1;
+printf("%d/n", count);
+}
+return (0);
+}
