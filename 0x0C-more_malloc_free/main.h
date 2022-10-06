b9243723f103c44607db23bc3ddb6adfb7ff6a3c
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-10-06 15:53:04 +0200

    Create main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * malloc_checked - checks malloc?
+ * @b: int to check? something
+ * Return: something void or pointer
+ */
+void *malloc_checked(unsigned int b);
+#endif
