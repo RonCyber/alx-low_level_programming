508621742d7857fdbcfcaa9bcd9693be3762f9cc
RonCyber <67389577+RonCyber@users.noreply.github.com>
2022-10-06 21:51:05 +0200

    Create 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * mul - multiplies num1 and num2
+ * @num1: multiplied
+ * @num2: multiplied to num1
+ * Return: 0 Always
+ */
+int mul(int num1, int num2)
+{
+int argc;
+int i;
+
+if (argc != 3 || check_number(n1) || check_number(n2))
+{
+_putchar ('Error');
+_putchar ('/n');
+exit (98);
+}
+ 
+i = num1 * num2
+
+	_putchar (i)
+_putchar ('/n');
+}
